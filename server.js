1efc4564e8c1651aa830378e2fad69cd5ee6a508
allamericenergy <support@ienergybill.com>
2026-05-12 20:38:23 +0530

    Fix server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin:"*",
 }));
 
-const PORT =  process.env.PORT || 3000;
+const PORT =  process.env.PORT || 5000;
 
 
 app.use('/api/auth', authRoutes);
@@ -34,9 +34,12 @@ app.get('/', (req, res) => {
 connectDB();
 
 
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
 /* app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
